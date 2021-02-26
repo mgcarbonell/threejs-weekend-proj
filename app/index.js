539f8671eb724183c48b6d42782c29438d016923
mgcarbonell <mgcarbonell@gmail.com>
2021-02-26 09:14:31 -0800

    Adds documentation in index.js

--- app/index.js
+++ app/index.js
@@ -1,6 +1,11 @@
 import * as THREE from 'three';
 
+// Set up  the scene
 const scene = new THREE.Scene();
+// Set up the camera: PerspectiveCamera is taking four attributes:
+// 1. Field of view (FOV): the extent of the scene that is on display in degrees
+// 2. The aspect ratio: The WIDTH divided by the HEIGHT
+// 3 & 4. The NEAR and FAR clipping plane. 
 const camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
